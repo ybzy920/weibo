3ff56d9f0b6664b74cc0a5dffafa3a78f5c4a5c8
YangBin <yss920@163.com>
2020-03-11 05:01:18 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::post('/users', 'UsersController@store')->name('users.store');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 Route::patch('/users', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
+
+Route::get('/login', 'SessionsController@create')->name('login');
+Route::post('/login', 'SessionsController@store')->name('login');
+Route::delete('/logout', 'SessionsController@destory')->name('logout');
