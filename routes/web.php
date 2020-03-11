b969758a724f85adc25419d825dd18da2cdffbab
YangBin <yss920@163.com>
2020-03-11 07:54:22 +0000

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/users/create', 'UsersController@create')->name('users.create');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::post('/users', 'UsersController@store')->name('users.store');
 Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
-Route::patch('/users', 'UsersController@update')->name('users.update');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
 
